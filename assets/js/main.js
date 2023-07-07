a959387fe8b3baa9eb45f9dcffc03df3e9cdb87d
777hanh777hanh <thanhthientran151100@gmail.com>
2023-07-07 19:11:57 +0700

    overView-left-side

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const navbar_items = $$('.navbar__item');
+const overViewDots = $$('.overView__pagination-item');
+const overViewValues = $$('.numbers-item__values');
 
 navbar_items.forEach((item) => {
     item.addEventListener('click', () => {
@@ -12,4 +14,23 @@ navbar_items.forEach((item) => {
 function toggleActiveNav(ele) {
     $('.navbar__item.active').classList.remove('active');
     ele.classList.add('active');
+}
+
+overViewDots.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        toggleActiveOverView(item, index);
+    });
+});
+overViewValues.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        toggleActiveOverView(item, index);
+    });
+});
+
+function toggleActiveOverView(ele, index) {
+    $('.overView__pagination-item.active').classList.remove('active');
+    $('.numbers-item__values.active').classList.remove('active');
+    ele.classList.add('active');
+    overViewValues[index].classList.add('active');
+    overViewDots[index].classList.add('active');
 }
